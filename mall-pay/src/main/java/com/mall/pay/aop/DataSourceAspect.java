73c8aaafe9ccdf581b3fd0911cde271e6238da46
wangzehui <18500507445@163.com>
2023-05-29 19:21:17 +0800

    修改

--- mall-pay/src/main/java/com/mall/pay/aop/DataSourceAspect.java
+++ mall-pay/src/main/java/com/mall/pay/aop/DataSourceAspect.java
@@ -1,6 +1,7 @@
 package com.mall.pay.aop;
 
 import com.mall.common.annotation.DataSource;
+import com.mall.common.config.DynamicDataSourceContextHolder;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
